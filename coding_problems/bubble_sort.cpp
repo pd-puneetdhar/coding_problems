a22709a3fbc657379509f6b4f3e5cf21c0041d3e
pd-puneetdhar <55623977+pd-puneetdhar@users.noreply.github.com>
2020-08-09 00:11:48 +0530

    Update bubble_sort.cpp

--- coding_problems/bubble_sort.cpp
+++ coding_problems/bubble_sort.cpp
@@ -1,9 +1,14 @@
-
 #include "assert.h"
+#include <iostream>
 #include <stdio.h>
 #include <random>
+#include <array> 
+#include <utility>
 
-void create_random_list_of_integers(std::list<unsigned int>& ints, unsigned int count, std::pair<unsigned int, unsigned int> range) {
+using int_list = std::array<unsigned int, 30>;
+
+void create_random_list_of_integers(int_list& ints, std::pair<unsigned int, unsigned int> range) {
+    unsigned int count = ints.size();
     assert(count);
     
     std::random_device randomizer;
@@ -11,15 +16,52 @@ void create_random_list_of_integers(std::list<unsigned int>& ints, unsigned int 
     
     for (unsigned int c = 0; c < count; c++) {
         auto num = dist(randomizer);
-        num.push_back
+        ints[c]=(num);
     }
 }
 
-void bubble_sort() {
+void print_list (int_list list_ints) {
+    for (auto int_i : list_ints) {
+        std::cout << int_i << ", " ;
+    }
+    std::cout << std::endl;
+}
+
+bool greater (unsigned int& first, unsigned int& second) {
+    return (first >= second);
+}
+
+bool swap (unsigned int& first, unsigned int& second) {
+    std::swap(first, second);
+}
+
+void bubble_sort(int_list& list_ints) {
     
+    auto size_of_list = list_ints.size();
+    
+    unsigned int full_pass_iterations = size_of_list - 1;
+    unsigned int sorted_places = 0;
+    
+    for (unsigned int full_passes_done = 0; full_passes_done < full_pass_iterations; full_passes_done++) {
+        for (unsigned int head = 0; head < (full_pass_iterations - full_passes_done); head++ ) {
+
+            auto& first = list_ints[head]; 
+            auto& second = list_ints[head+1];
+        
+            if (greater(first, second)) {
+                swap(first, second);
+            }
+        }
+    }
 }
 
 int main () {
-    create_random_list_of_integers(100, {0, 99});
-    bubble_sort();    
+    int_list l_ints  = {0}; 
+    
+    create_random_list_of_integers(l_ints, {0, 99});
+    std::cout << "UnSorted List" << std::endl;
+    print_list(l_ints);
+    bubble_sort(l_ints);    
+    std::cout << "Sorted List" << std::endl;
+    print_list(l_ints);
 }
