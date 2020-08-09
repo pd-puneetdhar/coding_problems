8d3ceb4eebef1cecba8c41db1abd5b01dadd8693
pd-puneetdhar <55623977+pd-puneetdhar@users.noreply.github.com>
2020-08-09 18:14:44 +0530

    Update sort.cpp

--- coding_problems/sort.cpp
+++ coding_problems/sort.cpp
@@ -2,147 +2,192 @@
 #include <iostream>
 #include <stdio.h>
 #include <random>
-#include <array> 
+#include <array>
 #include <utility>
 #include <functional>
 #include <memory>
 
-using int_list = std::array<unsigned int, 30>;
-using dist_range = std::pair<unsigned int, unsigned int>;
+using int_list = std::array<unsigned int , 30>;
+using dist_range = std::pair<unsigned int , unsigned int>;
+dist_range def_range = { 0, 99 };
 
-void create_random_list_of_integers(int_list& ints, dist_range range) {
-    unsigned int count = ints.size();
-    assert(count);
-    
+void create_random_list_of_integers( int_list& ints , dist_range range ) {
+    unsigned int count = ints.size( );
+    assert( count );
+
     std::random_device randomizer;
-    std::uniform_int_distribution<unsigned int> dist(range.first, range.second);
-    
-    for (unsigned int c = 0; c < count; c++) {
-        auto num = dist(randomizer);
-        ints[c]=(num);
+    std::uniform_int_distribution<unsigned int> dist( range.first , range.second );
+
+    for ( unsigned int c = 0; c < count; c++ ) {
+        auto num = dist( randomizer );
+        ints[c] = ( num );
     }
 }
 
-std::unique_ptr<int_list> int_randomizer(dist_range range) {
-    std::unique_ptr<int_list> ints = std::make_unique<int_list>() ;
-    create_random_list_of_integers(*ints.get(), range);
+std::unique_ptr<int_list> int_randomizer( dist_range range = def_range ) {
+    std::unique_ptr<int_list> ints = std::make_unique<int_list>( );
+    create_random_list_of_integers( *ints.get( ) , range );
     return ints;
 }
 
-void print_list (int_list& list_ints) {
-    for (auto int_i : list_ints) {
-        std::cout << int_i << ", " ;
+void print_list( int_list& list_ints ) {
+    for ( auto int_i : list_ints ) {
+        std::cout << int_i << ", ";
     }
     std::cout << std::endl;
 }
 
-bool smaller (unsigned int& first, unsigned int& second) {
-    return (first <= second);
+bool smaller( unsigned int& first , unsigned int& second ) {
+    return ( first <= second );
 }
 
-bool greater (unsigned int& first, unsigned int& second) {
-    return (first >= second);
+bool greater( unsigned int& first , unsigned int& second ) {
+    return ( first >= second );
 }
 
-bool swap (unsigned int& first, unsigned int& second) {
-    std::swap(first, second);
+void swap( unsigned int& first , unsigned int& second ) {
+    std::swap( first , second );
 }
 
-void bubble_sort(std::unique_ptr<int_list> p_ints) {
-    int_list& ints = *p_ints.get();
+void bubble_sort( std::unique_ptr<int_list> p_ints ) {
+    int_list& ints = *p_ints.get( );
 
     std::cout << __FUNCTION__ << ":: Unsorted list " << std::endl;
-    print_list(ints);
+    print_list( ints );
 
-    //inner loop compare head and head+1 and move head from begin to end-1 
-    //inner loop iterate over the integers from beginning to end - iteration 
+    //inner loop compare head and head+1 and move head from begin to end-1
+    //inner loop iterate over the integers from beginning to end - iteration
     //outer loop iterate size - 1 times and move temp end to end - iterations
-    
-    auto size_of_list = ints.size();
+
+    auto size_of_list = ints.size( );
     unsigned int full_pass_iterations = size_of_list - 1;
 
-    for (unsigned int full_passes_done = 0; full_passes_done < full_pass_iterations; full_passes_done++) {
-        for (unsigned int head = 0; head < (full_pass_iterations - full_passes_done); head++ ) {
+    for ( unsigned int full_passes_done = 0; full_passes_done < full_pass_iterations; full_passes_done++ ) {
+        for ( unsigned int head = 0; head < ( full_pass_iterations - full_passes_done ); head++ ) {
+            auto& first = ints[head];
+            auto& second = ints[head + 1];
 
-            auto& first = ints[head]; 
-            auto& second = ints[head+1];
-        
-            if (greater(first, second)) {
-                swap(first, second);
+            if ( greater( first , second ) ) {
+                swap( first , second );
             }
         }
     }
 
     std::cout << __FUNCTION__ << ":: Sorted list " << std::endl;
-    print_list(ints);
-
+    print_list( ints );
 }
 
-void selection_sort(std::unique_ptr<int_list> p_ints) {
-    int_list& ints = *p_ints.get();
+void selection_sort( std::unique_ptr<int_list> p_ints ) {
+    int_list& ints = *p_ints.get( );
 
     std::cout << __FUNCTION__ << ":: Unsorted list " << std::endl;
-    print_list(ints);
+    print_list( ints );
 
     //inner loop compare head element to all others in the the list head vs head+(1...end)
     //outer loop move head (begin...end - 1)
-    
-    auto size_of_list = ints.size();
-    unsigned int full_pass_iterations  = size_of_list - 1;
-    
-    for (unsigned int full_passes_done = 0; full_passes_done < full_pass_iterations; full_passes_done++) {
+
+    auto size_of_list = ints.size( );
+    unsigned int full_pass_iterations = size_of_list - 1;
+
+    for ( unsigned int full_passes_done = 0; full_passes_done < full_pass_iterations; full_passes_done++ ) {
         unsigned int head = full_passes_done;
-        for (unsigned int compare = head+1; compare <= full_pass_iterations; compare++) {
-            
+        for ( unsigned int compare = head + 1; compare <= full_pass_iterations; compare++ ) {
             auto& first = ints[head];
             auto& second = ints[compare];
-            
-            if (greater(first, second)) {
-                swap(first, second);
+
+            if ( greater( first , second ) ) {
+                swap( first , second );
             }
         }
     }
 
     std::cout << __FUNCTION__ << ":: Sorted list " << std::endl;
-    print_list(ints);
-
+    print_list( ints );
 }
 
-
-void selection_sort_perf_opt(std::unique_ptr<int_list> p_ints) {
-    int_list& ints = *p_ints.get();
+void selection_sort_perf_opt( std::unique_ptr<int_list> p_ints ) {
+    int_list& ints = *p_ints.get( );
     std::cout << __FUNCTION__ << ":: Unsorted list " << std::endl;
-    print_list(ints);
+    print_list( ints );
 
-    auto size_of_list = ints.size();
-    unsigned int full_pass_iterations  = size_of_list - 1;
-    
-    for (unsigned int full_passes_done = 0; full_passes_done < full_pass_iterations; full_passes_done++) {
+    auto size_of_list = ints.size( );
+    unsigned int full_pass_iterations = size_of_list - 1;
+
+    for ( unsigned int full_passes_done = 0; full_passes_done < full_pass_iterations; full_passes_done++ ) {
         unsigned int head = full_passes_done;
-        unsigned int minimum = head; 
+        unsigned int minimum = head;
 
-        for (unsigned int compare = head+1; compare <= full_pass_iterations; compare++) {
+        for ( unsigned int compare = head + 1; compare <= full_pass_iterations; compare++ ) {
             auto& first = ints[minimum];
             auto& second = ints[compare];
-            
-            if (greater(first, second)) {
+
+            if ( greater( first , second ) ) {
                 minimum = compare;
             }
         }
-        
-        if (head != minimum) {
-            swap(ints[head], ints[minimum]);
+
+        if ( head != minimum ) {
+            swap( ints[head] , ints[minimum] );
         }
     }
 
     std::cout << __FUNCTION__ << ":: Sorted list " << std::endl;
-    print_list(ints);
+    print_list( ints );
 }
 
-int main () {
+void insert_into_sorted_array( int_list& ints , unsigned int insert_this , unsigned int insert_at ) {
+    assert( insert_this > insert_at );
 
-    dist_range range = { 0, 99}; 
-    bubble_sort(int_randomizer(range));    
-    selection_sort(int_randomizer(range));
-    selection_sort_perf_opt(int_randomizer(range));
+    auto value_of_interest = ints[insert_this];
+    auto start_array_shift_at = insert_this;
+    auto end_array_shift_at = insert_at;
+
+    while ( start_array_shift_at > end_array_shift_at ) {
+        ints[start_array_shift_at] = ints[start_array_shift_at - 1];
+        start_array_shift_at--;
+    }
+
+    ints[end_array_shift_at] = value_of_interest;
+}
+
+void insertion_sort( std::unique_ptr<int_list> p_ints ) {
+    int_list& ints = *p_ints.get( );
+    std::cout << __FUNCTION__ << ":: Unsorted list " << std::endl;
+    print_list( ints );
+
+    //start with a sub-sorted list head and compare head + 1 with it.
+    //increase the sub-sorted list to head+(0...n-1) and while comparing the nth element with
+    //each element in the sorted sub array to find the place where it can be inserted
+
+    auto sorted_head = 0u;
+    auto sorted_end = 0u;
+    auto compare = sorted_end + 1u;
+
+    auto sorted_array_iterations = ints.size( ) - 1;
+
+    for ( sorted_end = 0; sorted_end < sorted_array_iterations; sorted_end++ ) {
+        compare = sorted_end + 1u;
+        for ( auto sorted_item_of_interest = sorted_end; sorted_item_of_interest >= sorted_head; sorted_item_of_interest-- ) {
+            if ( greater( ints[sorted_item_of_interest] , ints[compare] ) ) {
+                insert_into_sorted_array( ints , compare , sorted_item_of_interest );
+                compare = sorted_item_of_interest;
+            } else {
+                break;
+            }
+
+            if ( sorted_item_of_interest == 0 ) {
+                break;
+            }
+        }
+    }
+
+    std::cout << __FUNCTION__ << ":: Sorted list " << std::endl;
+    print_list( ints );
+}
+
+int main( ) {
+    bubble_sort( int_randomizer( ) );
+    selection_sort( int_randomizer( ) );
+    selection_sort_perf_opt( int_randomizer( ) );
+    insertion_sort( int_randomizer( ) );
 }
