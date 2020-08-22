fdc250e655769a6dc76c733d82865a88005069c5
pd-puneetdhar <55623977+pd-puneetdhar@users.noreply.github.com>
2020-08-23 01:34:09 +0530

    linked

--- linked_list.h
+++ linked_list.h
@@ -5,6 +5,7 @@ namespace linked_list {
 	class node {
 	public:
 		std::unique_ptr<node> get_next() { return m_next; }
+		T& get_data() { return m_data; }
 	private:
 		T m_data;
 		std::unique_ptr<node> m_next;
@@ -14,5 +15,12 @@ namespace linked_list {
 	class linked_list {
 		T node;
 
+		T* head{ nullptr };
+
+		void add(T& node) {
+			if (head == nullptr) {
+
+			}
+		}
 	};
 }
