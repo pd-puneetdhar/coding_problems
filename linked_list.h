c385139b0949530b8c3818e400b27e8f9190b29b
pd-puneetdhar <55623977+pd-puneetdhar@users.noreply.github.com>
2020-08-24 02:20:02 +0530

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #define LOG(x) std::cout << std::hex << __FUNCTION__ << " :" << x << std::dec << std::endl;
-
 namespace linked_list {
 	template <typename D>
 	class node {
@@ -25,7 +24,9 @@ namespace linked_list {
 		node* get_next( ) { return m_next; }
 		D& get_data( ) { return m_data; }
 		void set_next( node* n ) { m_next = n; }
-
+		void print( ) {
+			LOG( "Node data is " << get_data( ) );
+		}
 	private:
 		D m_data;
 		node* m_next = nullptr;
@@ -44,9 +45,12 @@ namespace linked_list {
 			}
 		}
 
+	private:
 		T* head { nullptr };
 
 	public:
+		T* get_head( ) { return head; }
+
 		void print( ) {
 			auto itr = head;
 
@@ -100,6 +104,29 @@ namespace linked_list {
 		}
 	};
 
+	//  practice questions
+	template<typename T , typename D>
+	T* find_middle_of_linked_list( linked_list<T , D>& ll ) {
+		LOG( "" );
+		auto head = ll.get_head( );
+		auto itr_fast = head;
+		auto itr_slow = head;
+
+		while ( itr_fast != nullptr ) {
+			if ( itr_fast->get_next( ) == nullptr ) { break; }
+			itr_fast = itr_fast->get_next( )->get_next( );
+			itr_slow = itr_slow->get_next( );
+		}
+
+		itr_slow->print( );
+		return itr_slow;
+	}
+
+	template<typename T , typename D>
+	void delete_middle_of_linked_list( linked_list<T , D>& ll ) {
+		//ll.free( find_middle_of_linked_list( ll ) );
+	}
+
 #define DEF_TEST(x) LOG(x)
 	void test_linked_list( ) {
 		DEF_TEST( "TEST -- NODE Create and Free" ) {
@@ -115,6 +142,14 @@ namespace linked_list {
 			ll.print( );
 			ll.free( 50 );
 			ll.free( 20 );
+			ll.add( 40 );
+			ll.add( 50 );
+			ll.add( 70 );
+			ll.add( -30 );
+
+			find_middle_of_linked_list( ll );
+			delete_middle_of_linked_list( ll );
+			ll.print( );
 			LOG( "Now the ll will be deleted" );
 		}
 	}
