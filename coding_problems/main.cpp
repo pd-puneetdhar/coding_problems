30d27118dd088306cf96ec558686839d02d3545b
pd-puneetdhar <55623977+pd-puneetdhar@users.noreply.github.com>
2020-08-23 12:59:21 +0530

    Linked list update

--- coding_problems/main.cpp
+++ coding_problems/main.cpp
@@ -5,5 +5,6 @@
 int main() {
 	sub_arrary_with_sum::test_find_sub_array_with_sum();
 	sort::test_sort();
+	linked_list::test_linked_list();
 	return 0;
 }
--- linked_list.h
+++ linked_list.h
@@ -1,26 +1,93 @@
 #pragma once
 
-namespace linked_list {
-	template <typename T>
-	class node {
+#define LOG(x) std::cout << __FUNCTION__ << " :" << x << std::endl;
+
+namespace linked_list
+{
+	template <typename D>
+	class node
+	{
+	public: // constructor
+		node() = delete;
+		node(D data) : m_data(data)
+		{
+			LOG("data = " << m_data);
+		}
+
 	public:
-		std::unique_ptr<node> get_next() { return m_next; }
-		T& get_data() { return m_data; }
+		static node* create_node(D data)
+		{
+			return new node(data);
+		}
+
+		static void free_node(node* n) {
+			delete n;
+			n = nullptr;
+		}
+
+		node* get_next() { return m_next; }
+		D& get_data() { return m_data; }
+		void set_next(node* n) { m_next = n; }
+
 	private:
-		T m_data;
-		std::unique_ptr<node> m_next;
+		D m_data;
+		node* m_next = nullptr;
 	};
 
-	template <typename T>
-	class linked_list {
-		T node;
-
+	template <typename T, typename D>
+	class linked_list
+	{
 		T* head{ nullptr };
 
-		void add(T& node) {
-			if (head == nullptr) {
+	public:
+		void print() {
 
+			auto itr = head;
+
+			while (itr != nullptr) {
+				std::cout << "Data = " << itr->get_data() << std::endl;
+				itr = itr->get_next();
 			}
 		}
+		T* end()
+		{
+			T* itr = head;
+			while (itr->get_next() != nullptr)
+			{
+				itr = itr->get_next();
+			}
+			return itr;
+		}
+
+		void add(D data)
+		{
+			if (head == nullptr)
+			{
+				head = node<D>::create_node(data);
+			}
+			else
+			{
+				if (auto last = end())
+				{
+					last->set_next(node<D>::create_node(data));
+				}
+			}
+		}
+
+		void free(T& arg_node) {
+			arg_node.free_node();
+		}
 	};
-}
+
+	void test_linked_list()
+	{
+		LOG("TEST NODE CREATION");
+		auto n = node<unsigned int>::create_node(30);
+		LOG("TEST LINKED LIST CREATION");
+		linked_list<node<unsigned int>, unsigned int> ll;
+		ll.add(10);
+		ll.add(20);
+		ll.add(30);
+		ll.print();
+	}
+} // namespace linked_list
